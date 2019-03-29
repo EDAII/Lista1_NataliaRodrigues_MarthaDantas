5669700e188f5b43ab64fe623a6e38c69d6bfcf1
Natália Rodrigues <nataliamrq@gmail.com>
2019-03-29 18:01:30 -0300

    Adding time counter.

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,12 @@
 #include <stdlib.h>
 #include <thread>
 #include <list>
+#include <iterator>
 #include <algorithm>
 #include <fstream>
 #include <string>
-
+#include <sys/types.h>
+#include <signal.h>
 #include <sys/time.h>
 #include <bits/stdc++.h>
 #include <sys/resource.h>
@@ -19,37 +21,34 @@ using json = nlohmann::json;
 vector <string> loadMunicipios();
 string getData();
 json cleanData(string text);
-string stringPattern(string s);
+
+void getEntries();
+void doWhatDadDo(pid_t child);
+void doWhatChildDo(pid_t dad);
+void putFile(string entry);
+void loadEntries();
 
 int countTime();
 void saveGuessing(list<pair<int,string>> guessing_list);
 int sequencialSearch(vector<string> municipios, string guessing);
 void countScore(vector<string> municipios, string guessing);
 void moveToforward(vector<string> municipios, int position);
+string stringPattern(string s);
 
 void showReport(vector<string> municipios, list<pair<int, string>> guessing_list);
 vector<pair<int, string>> getTopGuessed(list<pair<int, string>> guessing_list);
 double calcTime(const struct rusage *b, const struct rusage *a);
 
 int counter = 0;
+list<string> entries;
 
 int main() {
     list<pair<int, string>> guessing_list;
     vector <string> municipios = loadMunicipios();
 
-    guessing_list.push_back(make_pair(1, "N"));
-    guessing_list.push_back(make_pair(2, "N"));
-    guessing_list.push_back(make_pair(3, "N"));
-    guessing_list.push_back(make_pair(4, "N"));
-    guessing_list.push_back(make_pair(5, "N"));
-
-    showReport(municipios, guessing_list);
-/*    thread timer (countTime);
-    thread guessing (saveGuessing);
-
-    timer.join();
-    guessing.join();
-*/
+    getEntries();
+    loadEntries();
+    
     return 0;
 }
 
@@ -92,9 +91,56 @@ json cleanData(string text) {
     return j_filtered;
 }
 
-string stringPattern(string s) {
-    transform(s.begin(), s.end(), s.begin(), ::tolower);
-    return s;
+void getEntries(){
+    pid_t child = fork();
+
+    if(child > 0) {
+        doWhatDadDo(child);
+    }
+    else {
+        doWhatChildDo(getppid());
+    }
+}
+
+void doWhatDadDo(pid_t child) {
+    system("clear");
+    cout << "\nVocê tem 30s para colocar o máximo de cidades que conseguir lembrar: \n\n";
+
+    sleep(30);
+    kill(child, SIGTERM);
+
+}
+
+void doWhatChildDo(pid_t dad) {
+    string entry;
+
+    while(1) {
+        getline(cin, entry);
+        putFile(entry);
+    }
+    
+}
+
+void putFile(string entry) {
+    const char *c = entry.c_str();
+
+    FILE *fp = fopen("../src/entries.txt", "a+");
+    fprintf(fp, "%s\n", c);
+    fclose(fp);
+}
+
+void loadEntries() {
+    ifstream fp("../src/entries.txt");
+
+    string entry;
+    while(!fp.eof()){
+        getline(fp, entry);
+        entries.push_back(entry);
+    }
+
+    fp.close();
+    remove("../src/entries.txt");
+    
 }
 
 int countTime() {
@@ -154,6 +200,11 @@ void moveToforward(vector<string> municipios, int position) {
     }
 
     municipios[0] = aux;
+}
+
+string stringPattern(string s) {
+    transform(s.begin(), s.end(), s.begin(), ::tolower);
+    return s;
 }
 
 void showReport(vector <string> municipios, list<pair<int, string>> guessing_list) {
