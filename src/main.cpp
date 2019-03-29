28b96fc0ca35b4485eb9d0c5be18dd010d23d057
MarthaDs <marthadntas@gmail.com>
2019-03-28 21:22:01 -0300

    [FUNCTION] Functions: countTimer and saveGuessing in progress

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,38 @@
 #include <iostream>
+#include <unistd.h>
+#include <stdlib.h>
+#include <thread>
+#include <list>
+#include<string>
 
 using namespace std;
 
+int countTime();
+void saveGuessing();
 
 int main() {
+    thread timer (countTime);
+    thread guessing (saveGuessing);
 
+    timer.join();
+    guessing.join();
+
+return 0;
+}
+
+int countTime() {
+    int time = 60;
+    while(time >= 0) {
+        time--;
+        sleep(1);
+    }
     return 0;
+}
+
+void saveGuessing() {
+    string guessing;
+    list<string> guessing_list;
+    
+    cin >> guessing;
+    guessing_list.push_back(guessing);
 }
