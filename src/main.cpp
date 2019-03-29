dda53b961433198dcd93e08c8ba519a633f09610
MarthaDs <marthadntas@gmail.com>
2019-03-29 01:43:19 -0300

    [FUNCTION] Functions of sequencial search with sentinel and move to forward are done

--- src/main.cpp
+++ src/main.cpp
@@ -9,10 +9,16 @@
 using namespace std;
 
 int countTime();
-void saveGuessing();
+void saveGuessing(list<pair<int,string>> guessing_list);
 string stringPattern(string s);
+int sequencialSearch(vector<string> municipios, string guessing);
+void countScore(vector<string> municipios, string guessing);
+void moveToforward(vector<string> municipios, int position);
+
+int counter = 0;
 
 int main() {
+    list<pair<int, string>> guessing_list;
 /*    thread timer (countTime);
     thread guessing (saveGuessing);
 
@@ -31,20 +37,59 @@ int countTime() {
     return 0;
 }
 
-void saveGuessing() {
+void saveGuessing(list<pair<int,string>> guessing_list) {
     string guessing;
-    list<string> guessing_list;
     
     cin >> guessing;
     guessing = stringPattern(guessing);
 
-    guessing_list.push_back(guessing);
-    cout << guessing_list.front() << endl;
+    guessing_list.push_back(make_pair(0, guessing));
 
 }
 
 string stringPattern(string s) {
-    transform(s.begin(), s.end(), s.begin(), ::toupper);
-
+    transform(s.begin(), s.end(), s.begin(), ::tolower);
     return s;
 }
+
+int sequencialSearch(vector<string> municipios, string guessing) {
+    unsigned int i = 0;
+
+    municipios.push_back(guessing);
+
+    while(guessing != municipios[i]) {
+        i++;
+    }
+    if(i < (municipios.size() - 1)) {
+        return i;
+    }
+    else {
+        return -1;
+    }
+    
+}
+
+void countScore(vector<string> municipios, string guessing) {
+    int position = sequencialSearch(municipios, guessing);
+
+    if(position > 0) {
+        counter++;
+        moveToforward(municipios, position);
+    }
+    else {
+        // nothing to do
+    }
+}
+
+void moveToforward(vector<string> municipios, int position) {
+    string aux = municipios.front();
+
+    municipios.front() = municipios[position];
+
+    for(int i = position; i > 0; i--) {
+        municipios[i] = municipios[i-1];
+    }
+
+    municipios[0] = aux;
+}
+
